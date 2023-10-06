ad939b4c97ffc6aa7bb46d8666819bf76d29ea6f
jamie <95137262+jamiehax@users.noreply.github.com>
2023-10-06 10:25:22 -0400

    Create xcode app (#5)
    * adds code of conduct
    
    * creates the cookify app in xcode

---
+++ Cookify/Cookify/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Cookify
+//  The best app to eat food on in 2018
+//
+//  Created by Jamie Hackney, Jake Gilbert, Mihir Singh on 10/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Cookify!!!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cookify/Cookify/CookifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookifyApp.swift
+//  Cookify
+//
+//  Created by Jamie Hackney on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CookifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
