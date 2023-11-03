6444aa0e0a49f8ad70abee81cac5440e9958e07e
mihirBSingh <47934858+mihirBSingh@users.noreply.github.com>
2023-11-03 09:43:00 -0400

    Main feed UI (#15)
    * basic framework for ui as well as mock data struct for feed
    
    * committing some work before standup
    
    ---------
    
    Co-authored-by: Mihir Singh <mihirsingh@mihirs-mbp-2.middlebury.edu>

--- Cookify/Cookify/Code/Features/Feed/FeedView.swift
+++ Cookify/Cookify/Code/Features/Feed/FeedView.swift
@@ -7,9 +7,52 @@
 
 import SwiftUI
 
+struct Post: Decodable{
+    let id: Int
+    
+    let postProfile: String
+    let postTitle: String
+    let description: String
+    let imageURL: String
+}
+
+struct PostResponse{
+    let request: [Post]
+}
+
+struct MockData{
+    static let samplePost = Post(id: 001,
+                                 postProfile: "Meeks",
+                                 postTitle: "cookies",
+                                 description: "These are some wonderful cookies. I baked them today.",
+                                 imageURL: "String")
+    
+    static let samplePostList = [samplePost, samplePost, samplePost]
+}
+
+
 struct FeedView: View {
     var body: some View {
-        Text("Feed goes here")
+        NavigationView{
+            List(MockData.samplePostList, id: \.id){ Post in
+    
+                VStack(alignment: .leading){
+                    Text(Post.postTitle)
+                        .font(.title)
+                        .fontWeight(.medium)
+                    Text(Post.description)
+                        .font(.headline)
+                        .fontWeight(.light)
+                    HStack{
+                        Image("JT-Chocolate-Chip-Cookies-articleLarge")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                
+            }
+            .navigationTitle("What's Cooking:")
+        }
     }
 }
 
